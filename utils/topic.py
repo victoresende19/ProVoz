c721b6739e476ca891ad7115fee3c4c80c3108e6
Victor Resende <victoresende19@gmail.com>
2024-04-22 15:57:29 -0300

    :star: download :star:

--- utils/topic.py
+++ utils/topic.py
@@ -10,9 +10,9 @@ import streamlit as st
 
 @st.cache_data(show_spinner=False, ttl=24*3600, max_entries=5)
 def downloads_nlp():
-    import nltk
+    nltk.corpus.stopwords.words('portuguese')
     nltk.download('punkt')
-    nltk.download('stopwords')
+    # nltk.download('stopwords')
 
     spacy.cli.download("pt_core_news_sm")
     nlp = spacy.load('pt_core_news_sm')
