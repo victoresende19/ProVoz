ecb9fdd004d51828e32204c0518b68fcd3e6aba1
Victor Resende <victoresende19@gmail.com>
2024-04-22 20:51:03 -0300

    :star: topic info  :star:

--- utils/data.py
+++ utils/data.py
@@ -30,5 +30,5 @@ def data(type):
         response = requests.get(url, headers=headers)
         
         if response.status_code == 200:
-            data = pd.read_csv(StringIO(response.text))
+            data = pd.read_csv(StringIO(response.text), sep=';', on_bad_lines='skip')
             return data
